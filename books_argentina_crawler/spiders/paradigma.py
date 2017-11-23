116c0ed7ce27b250357bd47552ec0c12f208a0b7
Walther Lee <walthere.lee@gmail.com>
2017-11-23 13:21:48 -0800

    changes in ParadigmaSpdier encoding request

--- books_argentina_crawler/spiders/paradigma.py
+++ books_argentina_crawler/spiders/paradigma.py
@@ -5,6 +5,16 @@ import scrapy
 class ParadigmaSpider(scrapy.Spider):
     name = "paradigmalibros.com"
 
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.details_headers = {
+            "Host": "www.paradigmalibros.com",
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Connection": "keep-alive"
+        }
+
     def start_requests(self):
         urls = ['https://www.paradigmalibros.com/sitemap_' + str(i) + '.xml' for i in range(0,20)]
         for url in urls:
@@ -14,15 +24,6 @@ class ParadigmaSpider(scrapy.Spider):
         response.selector.register_namespace('d', 'http://www.sitemaps.org/schemas/sitemap/0.9')
         locs = response.selector.xpath("//d:loc/text()")
 
-        self.details_headers = {
-            "Host": "www.paradigmalibros.com",
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Connection": "keep-alive"
-        }
-
         for loc in locs:
             url = loc.extract()
             if "bookId" in url:
@@ -30,10 +31,10 @@ class ParadigmaSpider(scrapy.Spider):
                 yield scrapy.Request(url=url, callback=self.parse_details, headers=self.details_headers)
 
     def parse_details(self, response):
-        data={
+        data = {
             "url": response.url,
             "title": self.clean_text(response.selector.xpath('//div[@class="detInfoP"]/h1/text()').extract_first()),
-            #"content": self.clean_text(response.selector.xpath('//div/p[@itemprop="description"]/text()').extract_first()),
+            "content": "",
             "author": self.clean_text(response.selector.xpath("//div/span/p/a[@href]/text()").extract_first()),
             "editorial": self.clean_text(response.selector.xpath('//p[contains(text(),"Editorial")]/a[@title]/text()').extract_first()),
             "price": self.clean_price(response.selector.xpath('//div/p[@style]/text()').extract_first()),
@@ -43,6 +44,7 @@ class ParadigmaSpider(scrapy.Spider):
         yield data
 
     def clean_text(self, text):
+        print(text)
         if not isinstance(text, str):
             return "asd"
         text = re.sub("[\n\t]+", "", text)
