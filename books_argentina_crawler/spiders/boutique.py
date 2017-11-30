ad51347d1465e88735310febd19d9fecf4e0a4a3
an_sepulveda <vedaanam@gmail.com>
2017-11-30 10:09:08 -0800

    changes to ParadigmaSpider

--- books_argentina_crawler/spiders/boutique.py
+++ books_argentina_crawler/spiders/boutique.py
@@ -2,13 +2,12 @@ import re
 import scrapy
 
 
-class ParadigmaSpider(scrapy.Spider):
-    name = "paradigmalibros.com"
+class BoutiqueDelLibroSpider(scrapy.Spider):
+    name = "boutiquedellibro.com"
 
     def __init__(self, name=None, **kwargs):
         super().__init__(name, **kwargs)
         self.details_headers = {
-            "Host": "www.paradigmalibros.com",
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
@@ -16,35 +15,30 @@ class ParadigmaSpider(scrapy.Spider):
         }
 
     def start_requests(self):
-        urls = ['https://www.paradigmalibros.com/sitemap_' + str(i) + '.xml' for i in range(0,20)]
+        urls = ['https://www.boutiquedellibro.com.ar/sitemap' + str(i) + '.xml' for i in range(0,6)]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        response.selector.register_namespace('d', 'http://www.sitemaps.org/schemas/sitemap/0.9')
+        response.selector.register_namespace('d', 'http://www.google.com/schemas/sitemap/0.9')
         locs = response.selector.xpath("//d:loc/text()")
 
         for loc in locs:
             url = loc.extract()
-            if "bookId" in url:
-                url = url.replace("http:", "https:")
-                yield scrapy.Request(url=url, callback=self.parse_details, headers=self.details_headers)
+            print(url)
+            url = url.replace("http:", "https:")
+            yield scrapy.Request(url=url, callback=self.parse_details, headers=self.details_headers)
 
     def parse_details(self, response):
         data = {
             "url": response.url,
-            "title": self.clean_text(response.selector.xpath('//div[@class="detInfoP"]/h1/text()').extract_first()),
+            "title": self.clean_text(response.selector.xpath('//div/h1/a[@href]/text()').extract_first()),
             "content": "",
-            "author": self.clean_text(response.selector.xpath("//div/span/p/a[@href]/text()").extract_first()),
-            "editorial": self.clean_text(response.selector.xpath('//p[contains(text(),"Editorial")]/a[@title]/text()').extract_first()),
-            "ISBN": self.clean_text(response.selector.xpath('//p/span/text()').extract_first().split(":")[-1].strip())
+            "author": self.clean_text(response.selector.xpath("//p/span/a[@href]/text()").extract_first()),
+            "editorial": self.clean_text(response.selector.xpath('//li/span/a[@href]/text()').extract_first()),
+            "price_arg": self.clean_price(response.selector.xpath('//div/p/span[@class]/text()').extract_first()),
+            "ISBN": self.clean_text(response.selector.xpath('//li/span[@itemprop="isbn"]/text()').extract_first().split(":")[-1].strip())
         }
-
-        price = self.clean_price(response.selector.xpath('//div/p[@style]/text()').extract_first()),
-        price_arg, price_usd = price.split("||")
-
-        data["price_arg"] = self.clean_price(price_arg)
-        data["price_usd"] = self.clean_price(price_usd)
 
         yield data
 
